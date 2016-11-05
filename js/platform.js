21a95618015f63bb171c6aa4af9a1ec2ffafa0ba
Justin Mayfield <tooker@gmail.com>
2016-11-04 18:01:44 -0600

    Locale hacks

--- js/platform.js
+++ js/platform.js
@@ -6,6 +6,8 @@
 
     window.platform = window.platform || {};
 
+    var messages = __relay_locale();
+
     /*
     window.platform.navigator = (function () {
         var self = {},
@@ -146,11 +148,42 @@
 
     // Translate
     window.i18n = function(message, substitutions) {
-        console.warn("TRANSLATION IS BROKEN", message);
-        if (substitutions !== undefined) {
-            debugger; // Get into it.
+        let entry = messages[message];
+        if (entry === undefined) {
+            console.warn("Translation missing", message);
+            return '';
         }
-        return 'TRANSLATION_BROKEN(' + message + ')';
+        let subs = {};
+        if (entry.placeholders !== undefined) {
+            Object.keys(entry.placeholders).forEach(function(key) {
+                let x = entry.placeholders[key];
+                if (x.content.indexOf('$') === -1) {
+                    subs[key] = x.content;
+                } else {
+                    let index = Number(x.content.slice(1)) - 1;
+                    subs[key] = substitutions[index];
+                }
+            });
+        }
+        let parts = [];
+        entry.message.split('$').forEach(function(x, i) {
+            if (i % 2) {
+                if (x === '') {
+                    parts.push('$');  // $$ == literal %
+                } else {
+                    let sub = subs[x];
+                    if (sub === undefined) {
+                        console.error('Missing substitution:', x, subs);
+                        throw 'bad substitution';
+                    } else {
+                        parts.push(sub);
+                    }
+                }
+            } else {
+                parts.push(x);
+            }
+        });
+        return parts.join('');
     };
 
     i18n.getLocale = function() {

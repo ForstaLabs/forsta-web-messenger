87e22cd8e3002569ae672c3ec9d3c2e398cf4e90
Justin Mayfield <tooker@gmail.com>
2017-02-19 12:38:04 -0700

    Getting grunt build working
    I need to make tweaks to the textsecure "lib" which is compiled by
    grunt. Sigh.

--- start.js
+++ start.js
@@ -4,12 +4,16 @@ const serveStatic = require('serve-static');
 const process = require('process');
 
 const PORT = Number(process.env.PORT) || 8000;
-
 const app = express();
 
 app.use(morgan('dev'));
 app.use(serveStatic('.', {
     index: ['inbox.html']
 }));
+app.get('/env', function(req, res) {
+    res.json({
+        "SUPERMAN_NUMBER": process.env.SUPERMAN_NUMBER || '+12086391772'
+    });
+});
 
 app.listen(PORT);

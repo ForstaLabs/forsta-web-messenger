dc4f3904efc007fa5879a7fc206d8a59f3951c2a
Justin Mayfield <tooker@gmail.com>
2017-02-19 13:14:24 -0700

    No idea here.

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,66 +151,7 @@ module.exports = function(grunt) {
         ]
       }
     },
-    watch: {
-      sass: {
-        files: ['./stylesheets/*.scss'],
-        tasks: ['sass']
-      },
-      libtextsecure: {
-        files: ['./libtextsecure/*.js', './libtextsecure/storage/*.js'],
-        tasks: ['concat:libtextsecure']
-      },
-      dist: {
-        files: ['<%= dist.src %>', '<%= dist.res %>'],
-        tasks: ['copy_dist']
-      },
-      scripts: {
-        files: ['<%= jshint.files %>', './js/**/*.js'],
-        tasks: ['jshint']
-      },
-      style: {
-        files: ['<%= jscs.all.src %>', './js/**/*.js'],
-        tasks: ['jscs']
-      },
-    },
-    connect: {
-      server: {
-        options: {
-          base: '.',
-          port: 9999
-        }
-      }
-    },
-    'saucelabs-mocha': {
-      all: {
-        options: {
-          urls: [
-            'http://127.0.0.1:9999/test/index.html',
-            'http://127.0.0.1:9999/libtextsecure/test/index.html',
-          ],
-          build: process.env.TRAVIS_JOB_ID,
-          browsers: [
-            { browserName: 'chrome', version: '41' },
-            { platform: 'linux', browserName: 'firefox', version: '34' }
-          ],
-          testname: 'TextSecure-Browser Tests',
-          'max-duration': 300,
-          statusCheckAttempts: 200
-        }
-      }
-    },
-    exec: {
-      'tx-pull': {
-        cmd: 'tx pull'
-      }
-    },
     gitinfo: {} // to be populated by grunt gitinfo
-  });
-
-  Object.keys(grunt.config.get('pkg').devDependencies).forEach(function(key) {
-    if (/^grunt(?!(-cli)?$)/.test(key)) {  // ignore grunt and grunt-cli
-      grunt.loadNpmTasks(key);
-    }
   });
 
   // Transifex does not understand placeholders, so this task patches all non-en
@@ -235,10 +176,6 @@ module.exports = function(grunt) {
     });
   });
 
-  grunt.registerTask('tx', ['exec:tx-pull', 'locale-patch']);
-  grunt.registerTask('dev', ['default', 'connect', 'watch']);
-  grunt.registerTask('test', ['jshint', 'jscs', 'connect', 'saucelabs-mocha']);
   grunt.registerTask('copy_dist', ['gitinfo', 'copy']);
   grunt.registerTask('default', ['concat', 'sass', 'copy_dist']);
-
 };

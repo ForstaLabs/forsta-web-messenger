a8c2600d20a79c4491b2b2327593019d987cf6da
Justin Mayfield <tooker@gmail.com>
2017-02-19 13:33:51 -0700

    Use grunt-sass (!contrib)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-gitinfo');
 
   var bower = grunt.file.readJSON('bower.json');

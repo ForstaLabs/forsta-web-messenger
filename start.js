5ab12ba06390bdca51d522a8caca2023323f7622
Justin Mayfield <tooker@gmail.com>
2017-05-09 16:32:40 -0600

    Add support for embedding ccsm-ui.
    Move default static file serving happen at /m and optionally
    reverse proxy to an external ccsm-ui server if `RELAY_CCSM_EMBED=1`.

--- start.js
+++ start.js
@@ -4,7 +4,6 @@ const serveStatic = require('serve-static');
 const process = require('process');
 
 const PORT = Number(process.env.PORT) || 1080;
-const app = express();
 const env_clone = [
     'ANDROID_APP_URL',
     'IOS_APP_URL',
@@ -16,14 +15,30 @@ for (const x of env_clone) {
 }
 const root = process.env.DEVMODE === "1" ? '.' : 'dist';
 
-
+const app = express();
 app.use(morgan('dev'));
-app.use(serveStatic(root, {
+const siteRouter = express.Router();
+siteRouter.use(serveStatic(root, {
     index: ['inbox.html']
 }));
-
-app.get('/env.js', function(req, res) {
+siteRouter.get('/env.js', function(req, res) {
     res.send(`window.forsta_env = ${JSON.stringify(env)}`);
 });
+
+if (process.env.RELAY_CCSM_EMBED === "1") {
+    const httpProxy = require('http-proxy');
+    const proxy = httpProxy.createProxyServer({
+        target: process.env.RELAY_CCSM_URL || 'https://ccsm-dev.forsta.io',
+        changeOrigin: true
+    })
+    app.all(['/*'], function (req, res) {
+        //req.url = req.url.replace(/^\/m/, '/');
+        proxy.web(req, res);
+    });
+}
+app.use(['/m', '/m/*'], siteRouter);
+
+
+
 
 app.listen(PORT);

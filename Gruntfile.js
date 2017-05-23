d8c04451c0e4104ab057f025a54a64e3fa408a6d
Justin Mayfield <tooker@gmail.com>
2017-05-23 13:28:54 -0600

    Infinite journey of the real.
    Make the things more live better.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+// vim: et sw=2 ts=2
 const fs = require('fs');
 const path = require('path');
 
@@ -84,6 +85,62 @@ module.exports = function(grunt) {
         dest: `${static_dist}/lib/textsecure.js`
       },
 
+      app_main: {
+        src: [
+          'compat.js',
+          'ccsm.js',
+          'database.js',
+          'debugLog.js',
+          'storage.js',
+          'signal_protocol_store.js',
+          'notifications.js',
+          'delivery_receipts.js',
+          'read_receipts.js',
+          'libphonenumber-util.js',
+          'models/messages.js',
+          'models/conversations.js',
+          'models/blockedNumbers.js',
+          'expiring_messages.js',
+          'i18n.js',
+          'registration.js',
+          'conversation_controller.js',
+          'panel_controller.js',
+          'emoji_util.js',
+          'views/whisper_view.js',
+          'views/debug_log_view.js',
+          'views/toast_view.js',
+          'views/attachment_preview_view.js',
+          'views/file_input_view.js',
+          'views/list_view.js',
+          'views/conversation_list_item_view.js',
+          'views/conversation_list_view.js',
+          'views/contact_list_view.js',
+          'views/recipients_input_view.js',
+          'views/new_group_update_view.js',
+          'views/attachment_view.js',
+          'views/key_conflict_dialogue_view.js',
+          'views/error_view.js',
+          'views/timestamp_view.js',
+          'views/message_view.js',
+          'views/key_verification_view.js',
+          'views/message_detail_view.js',
+          'views/message_list_view.js',
+          'views/group_member_list_view.js',
+          'views/recorder_view.js',
+          'views/conversation_view.js',
+          'views/conversation_search_view.js',
+          'views/hint_view.js',
+          'views/main_view.js',
+          'views/confirmation_dialog_view.js',
+          'views/identicon_svg_view.js',
+          'views/settings_view.js',
+          'foundation.js',
+          'main.js'
+        ].map(x => add_prefix('app', x)),
+        dest: `${static_dist}/app/main.js`
+      },
+
+      /* legacy */
       app_inbox: {
         src: [
           'compat.js',
--- app/main.js
+++ app/main.js
@@ -1,11 +1,31 @@
-/*global $, Whisper, Backbone, textsecure, extension*/
 /*
  * vim: ts=4:sw=4:expandtab
  */
-(function () {
+(async function () {
     'use strict';
 
-    var view;
+    $('.ui.dropdown').dropdown();
+
+    $('#toggle-nav').on('click', ev => {
+      const nav = $('#forsta-app nav');
+      const icon = $('#toggle-nav i.icon');
+      if (nav.width() > 100) {
+          nav.width(80);
+          icon.removeClass('left');
+          icon.addClass('right');
+      } else {
+          nav.width(300);
+          icon.removeClass('right');
+          icon.addClass('left');
+      }
+    });
+
+    $('#forsta-app > nav table thead').on('click', ev => {
+      const el = $(ev.currentTarget);
+      const body = el.next('tbody');
+      body.toggle();
+    });
+
 
     storage.onready(function() {
         if (Whisper.Registration.isDone()) {
@@ -17,16 +37,15 @@
         }
     });
 
-    ConversationController.updateInbox().then(function() {
-        if (view) { view.remove(); }
-        var $body = $('body', document).empty();
-        view = new Whisper.InboxView({window: window});
-        view.$el.prependTo($body);
-        window.openConversation = function(conversation) {
-            if (conversation) {
-                view.openConversation(null, conversation);
-            }
-        };
-        openConversation(getOpenConversation());
-    });
+    await ConversationController.updateInbox();
+    var view;
+    var $body = $(document.body);
+    view = new Forsta.MainView({window: window});
+    view.$el.prependTo($body);
+    window.openConversation = function(conversation) {
+        if (conversation) {
+            view.openConversation(null, conversation);
+        }
+    };
+    openConversation(getOpenConversation());
 }());

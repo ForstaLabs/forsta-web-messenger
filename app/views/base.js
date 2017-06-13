ecb62952f5b85ad83e12ec8cb779c4b84fd0ac29
Justin Mayfield <tooker@gmail.com>
2017-06-13 12:22:38 -0600

    Add template root element hoisting.
    Let template views hoist their root node(s) to the view's $el prop.

--- app/views/base.js
+++ app/views/base.js
@@ -6,13 +6,27 @@
 
     window.F = window.F || {};
 
+    const FViewOptions = [
+        'templateUrl',
+        'templateRootAttach' // Use template's root element(s) for the $el prop.
+    ];
+
     F.View = Backbone.View.extend({
 
         constructor: function(options) {
-            _.extend(this, _.pick(options, ['templateUrl', 'templatePartials']));
+            _.extend(this, _.pick(options, FViewOptions));
             return Backbone.View.prototype.constructor.apply(this, arguments);
         },
 
+        /* Defer creation of $el if configured to attach template to root. */
+        _ensureElement: function() {
+            if (this.templateRootAttach && !this._rendered) {
+                return; // Defer element assignment to render().
+            } else {
+                Backbone.View.prototype._ensureElement.call(this);
+            }
+        },
+            
         delegateEvents: function() {
             if (this._rendered) {
                 return Backbone.View.prototype.delegateEvents.call(this);
@@ -27,7 +41,23 @@
             }
             if (this.template) {
                 const attrs = _.result(this, 'render_attributes', {});
-                this.$el.html(this.template(attrs));
+                const html = this.template(attrs);
+                if (this.templateRootAttach) {
+                    /* Copypasta from _ensureElement to graft extr attrs
+                     * onto our new root el. */
+                    const el_attrs = _.extend({}, _.result(this, 'attributes'));
+                    if (this.id) {
+                        el_attrs.id = _.result(this, 'id');
+                    }
+                    if (this.className) {
+                        el_attrs['class'] = _.result(this, 'className');
+                    }
+                    const $el = $(html);
+                    $el.attr(el_attrs);
+                    this.setElement($el, /*delegateEvents*/ false);
+                } else {
+                    this.$el.html(html);
+                }
             }
             this._rendered = true;
             this.delegateEvents();

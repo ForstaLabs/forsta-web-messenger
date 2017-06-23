6cf851636629f78083ca3f08b04439af5a7484cd
ms2300 <matt.sewall@gmail.com>
2017-06-23 14:24:16 -0600

    current, next changes will attempt DOM purify

--- app/util.js
+++ app/util.js
@@ -74,7 +74,7 @@
         });
     };
 
-    const code_block = /```$([\s\S]*?)^```/gm;
+    const code_block = /```([\s\S]*?)```/gm;
     const a = /((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)))(">(.*)<\/a>)?/gi;
     const styles = {
         samp: /`(\S.+?)`/g,

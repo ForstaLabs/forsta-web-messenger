a1dc9cbbcea0def505c2da1aa0d2d442cffac6f5
Justin Mayfield <tooker@gmail.com>
2017-06-23 16:49:41 -0600

    Remove timing debug from storage api

--- app/storage.js
+++ app/storage.js
@@ -4,12 +4,6 @@
 ;(function() {
     'use strict';
 
-    window.times = {
-        put: 0,
-        get: 0,
-        remove: 0
-    }
-
     function encodeArrayBuffer(buf) {
         return btoa(String.fromCharCode.apply(null, new Uint8Array(buf)));
     }
@@ -24,9 +18,8 @@
         return buf;
     }
 
-    window.storage = {
+    self.storage = {
         put: function(key, value) {
-            const start = performance.now()
             if (value === undefined) {
                 throw new Error("Tried to store undefined");
             }
@@ -36,40 +29,24 @@
                 conv = 'AB1';
             }
             localStorage.setItem(key, JSON.stringify({conv, value}));
-            times.put += performance.now() - start;
         },
 
         get: function(key, defaultValue) {
-            const start = performance.now()
             const raw = localStorage.getItem(key);
             if (raw === null) {
-                times.get += performance.now() - start;
                 return defaultValue;
             }
             const data = JSON.parse(raw);
             if (data.conv === 'AB1') {
                 data.value = decodeArrayBuffer(data.value);
             }
-            times.get += performance.now() - start;
             return data.value;
         },
 
-        remove: function(key) {
-            const start = performance.now()
-            localStorage.removeItem(key);
-            times.remove += performance.now() - start;
-        },
-
-        onready: function(callback) {
-            throw new Error("Don't use tis");
-            callback();
-        },
-
-        fetch: async function() {
-        }
+        remove: localStorage.removeItem.bind(localStorage)
     };
 
-    window.textsecure = window.textsecure || {};
-    window.textsecure.storage = window.textsecure.storage || {};
-    window.textsecure.storage.impl = window.storage;
+    self.textsecure = self.textsecure || {};
+    textsecure.storage = textsecure.storage || {};
+    textsecure.storage.impl = self.storage;
 })();

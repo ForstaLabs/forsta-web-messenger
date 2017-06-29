938bb003ee200adbd4ea9f2f7e44d5fc9e36d9cd
Justin Mayfield <tooker@gmail.com>
2017-06-28 19:19:58 -0600

    Checkpoint for CCSM/Backbone sync

--- app/models/ccsm.js
+++ app/models/ccsm.js
@@ -6,17 +6,26 @@
 
     self.F = self.F || {};
 
-    /* Perform CCSM API calls for sync. */
-    F.CCSMModel = Backbone.Model.extend({
-        sync: function(method, model, options) {
-            debugger;
-        }
-    });
+    const API = F.ccsm.getConfig().API;
 
-    F.CCSMCollection = Backbone.Collection.extend({
-        sync: function(method, collection, options) {
-            debugger;
-        }
-    });
+    const syncMixin = {
+        sync: async function(method, collection, options) {
+            /* CCSM setup for API calls.  The options dict will be passed to
+             * `jQuery.ajax`. */
+            options.headers = options.headers || {};
+            options.headers.Authorization = `JWT ${API.TOKEN}`;
+            return await Backbone.sync(method, collection, options).promise();
+        },
 
+        url: function() {
+            return API.URLS.BASE + this.urn;
+        },
+
+        parse: function(resp, options) {
+            return resp.results;
+        }
+    };
+
+    F.CCSMModel = Backbone.Model.extend(syncMixin);
+    F.CCSMCollection = Backbone.Collection.extend(syncMixin);
 })();
--- app/models/users.js
+++ app/models/users.js
@@ -7,9 +7,37 @@
     self.F = self.F || {};
 
     F.User = F.CCSMModel.extend({
+        urn: '/v1/user/'
     });
 
-    F.UsersCollection = Backbone.Collection.extend({
+    F.UsersCollection = F.CCSMCollection.extend({
         model: F.User,
+        urn: '/v1/user/'
     });
+
+    F.users = new F.UsersCollection();
+
+    (async function() {
+        try {
+            var c = await F.users.fetch();
+        } catch(e) {
+            debugger;
+        }
+        const m = new F.User({id: 111});
+        const m2 = new F.User({id: '222'});
+        try {
+            debugger;
+            await m.fetch();
+        } catch(e) {
+            debugger;
+        }
+        try {
+            await m2.fetch();
+        } catch(e) {
+            debugger;
+        }
+        debugger;
+        debugger;
+    });
+
 })();

3efdd04d2ba0c3b1e0823e77c185b55a3e848e8e
Justin Mayfield <tooker@gmail.com>
2017-06-30 19:23:47 -0600

    Add easter egg file for "nesting" r&d tools and tricks.

---
+++ app/easter.js
@@ -0,0 +1,37 @@
+/*
+ * vim: ts=4:sw=4:expandtab
+ */
+(function () {
+    'use strict';
+
+    self.F = self.F || {};
+    F.easter = {};
+
+    F.easter.registerSingle = async function(number) {
+        let phone = number.toString().replace(/[\.-\s]/g, '');
+        const buf = [];
+        if (!phone.startsWith('+')) {
+            buf.push('+');
+        }
+        if (!phone.startsWith('1')) {
+            buf.push('1');
+        }
+        buf.push(phone);
+        phone = buf.join('');
+
+        const am = await F.foundation.getAccountManager();
+        am.requestSMSVerification(phone);
+        const $el = $('<div class="ui modal"><div class="ui segment">' +
+                      '<div class="ui input action">' +
+                      '<input type="text" placeholder="Verification Code..."/>' +
+                      '<button class="ui button">Register</button>' +
+                      '</div></div></div>');
+        $el.on('click', 'button', async function() {
+            const code = $el.find('input').val().replace(/[\s-]/g, '');
+            await am.registerSingleDevice(phone, code);
+            $el.modal('hide');
+        });
+        $('body').append($el);
+        $el.modal('setting', 'closable', false).modal('show');
+    };
+})();

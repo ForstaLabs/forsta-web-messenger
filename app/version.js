1dfe895c3f7016ef63ac1f0e347cf8dbbd44e371
Justin Mayfield <tooker@gmail.com>
2017-07-31 12:10:23 -0600

    Brand !prod sites with R&D Sample

--- app/version.js
+++ app/version.js
@@ -1,3 +1,7 @@
 self.F = self.F || {};
 F.version = '0.3.0';
 F.product = 'ForstaWeb';
+
+if (!forsta_env || forsta_env.STACK_ENV !== 'prod') {
+    $('head').append('<link rel="stylesheet" href="/@static/stylesheets/dev.css" type="text/css"/>');
+}

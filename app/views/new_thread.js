69644ea9b24a9193fd26cda5bc21d771cefe02ae
Justin Mayfield <tooker@gmail.com>
2017-09-04 16:38:16 -0600

    Perhaps a workable design for starting a convo.

--- app/views/new_thread.js
+++ app/views/new_thread.js
@@ -18,25 +18,35 @@
             this.$panel = $('#f-new-thread-panel');
             this.$startNew = $('.f-start-new');
             this.$startNew.on('click', this.togglePanel.bind(this));
-            this.$dropdown = this.$panel.find('.dropdown');
+            this.$dropdown = this.$panel.find('.f-start-dropdown');
+            this.$panel.find('.f-header-menu .ui.dropdown').dropdown();
             this.$menu = this.$dropdown.find('.menu .menu');
-            this.$buttons = this.$panel.find('.ui.buttons .button');
+            this.$buttons = this.$panel.find('.ui.button');
             this.$startButton = this.$panel.find('.f-start-button');
             this.$startButton.on('click', this.onStartClick.bind(this));
             this.$clearButton = this.$panel.find('.f-clear-button');
             this.$clearButton.on('click', this.onClearClick.bind(this));
             this.$searchInput = this.$panel.find('input[name="search"]');
+            this.$panel.find('.ui.menu > .item[data-tab]').tab();
             // Must use event capture here...
             this.$searchInput[0].addEventListener('keydown', this.onKeyDown.bind(this), true); // XXX
-            this.$panel.find('.ui.search').search(); // XXX
-            this.$dropdown.dropdown({
-                fullTextSearch: true,
+            this.dropdown = this.$dropdown.dropdown.bind(this.$dropdown);
+            this.dropdown({
+                fullTextSearch: 'exact',
                 onChange: this.onSelectionChange.bind(this),
                 onHide: () => false, // Always active.
                 onLabelCreate: this.onLabelCreate,
+                onNoResults: this.onDropdownNoResults.bind(this),
             });
+            this.$announcement = this.$panel.find('.ui.checkbox');
+            this.$announcement.checkbox();
             await this.loadData();
             return this;
+        },
+
+        onDropdownNoResults: function(searchValue) {
+            console.warn("NO RESULTS", searchValue);
+            return true;
         },
 
         onLabelCreate: function(value, html) {
@@ -56,9 +66,8 @@
 
         showPanel: function() {
             $('nav > .ui.segment').scrollTop(0);
-            this.$dropdown.dropdown('show');
-            this.$dropdown.dropdown('restore defaults');
-            this.$searchInput.val('').focus();
+            this.dropdown('show');
+            this.resetDropdown(true);
             this.$startNew.find('i.icon.primary').removeClass('plus').addClass('minus');
             this.$startNew.find('i.icon.secondary').removeClass('pencil').addClass('minus');
             this.$panel.css({
@@ -79,17 +88,48 @@
                     maxHeight: '0'
                 });
                 this.$panel.css('max-height', '');
-                this.$dropdown.dropdown('restore defaults');
-                this.$startButton.find('i.icon').removeClass('loading notched circle').addClass('right arrow');
+                this.resetDropdown();
+                this.$startButton.find('i.icon').removeClass('loading notched circle').addClass('play');
                 this.$startNew.find('i.icon.primary').removeClass('minus').addClass('plus');
                 this.$startNew.find('i.icon.secondary').removeClass('minus').addClass('pencil');
             });
         },
 
+        resetDropdown: function(focus) {
+            this.$panel.find('.ui.dropdown').dropdown('restore defaults');
+            this.resetSearch(focus);
+        },
+
+        resetSearch: function(focus) {
+            this.$searchInput.val('');
+            this.dropdown('filter', '');
+            this.$dropdown.find('.scrolling.menu').scrollTop(0);
+            if (focus) {
+                this.dropdown('focusSearch');
+            }
+        },
+
         onKeyDown: async function(ev) {
-            if (ev.ctrlKey && ev.keyCode === /*enter*/ 13) {
-                ev.preventDefault();
-                await this.onStartClick();
+            if (ev.keyCode === /*enter*/ 13) {
+                if (ev.ctrlKey) {
+                    ev.preventDefault();
+                    await this.onStartClick();
+                } else if (this.dropdown('has allResultsFiltered')) {
+                    let tag = this.dropdown('get query');
+                    if (!tag.startsWith('@')) {
+                        tag = '@' + tag;
+                    }
+                    if (!tag.match(/^[^\s]+$/)) {
+                        console.warn("Not a tag:", tag);
+                        return;
+                    }
+                    ev.preventDefault();
+                    ev.stopPropagation();
+                    const $item = $(`<div class="item" data-value="${tag}">` +
+                                    `<div class="slug">${tag}</div></div>`);
+                    this.dropdown('set selected', tag, $item);
+                    this.resetSearch();
+                }
             }
         },
 
@@ -126,7 +166,7 @@
         },
 
         onSelectionChange: function() {
-            const raw = this.$dropdown.dropdown('get value');
+            const raw = this.dropdown('get value');
             if (raw.trim()) {
                 this.$buttons.removeClass('disabled');
                 this.$startButton.addClass('primary');
@@ -134,11 +174,11 @@
                 this.$startButton.removeClass('primary');
                 this.$buttons.addClass('disabled');
             }
-            this.$searchInput.val('').focus();
+            //this.resetSearch();
         },
 
         onStartClick: async function() {
-            this.$startButton.find('.icon').removeClass('right arrow').addClass('loading notched circle');
+            this.$startButton.find('.icon').removeClass('play').addClass('loading notched circle');
             try {
                 await this.startThread();
             } finally {
@@ -147,17 +187,18 @@
         },
 
         onClearClick: async function() {
-            this.$dropdown.dropdown('restore defaults');
+            this.resetDropdown(true);
         },
 
         startThread: async function() {
-            const raw = this.$dropdown.dropdown('get value');
+            const raw = this.dropdown('get value');
             if (!raw || !raw.trim().length) {
                 return;
             }
-
             const threads = F.foundation.getThreads();
-            const thread = await threads.ensure(raw, {type: 'conversation'});
+            const is_announcement = this.$panel.find('input[name="threadType"]').val() === 'announcement';
+            const type = is_announcement ? 'announcement' : 'conversation';
+            const thread = await threads.ensure(raw, {type});
             F.mainView.openThread(thread);
         }
     });

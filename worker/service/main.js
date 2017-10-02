b9bf80a402a8f2fb163fd510f3cb292fab532c95
Justin Mayfield <tooker@gmail.com>
2017-10-02 13:05:46 -0600

    Make firebase init in serviceworker optional.
    It needs to check for the proper config before running.

--- worker/service/main.js
+++ worker/service/main.js
@@ -2,8 +2,6 @@
 
 self.F = self.F || {};
 
-firebase.initializeApp(F.env.FIREBASE_CONFIG);
-
 addEventListener('install', function(ev) {
     skipWaiting(); // Force controlled clients to use us right away.
 });
@@ -27,7 +25,7 @@ async function messageDrain() {
     if (!_init) {
         console.info('Starting messaging foundation...');
         _init = true;
-        const userId = location.search.split('?id=')[1]; // XXX
+        const userId = location.search.split('?id=')[1];
         await F.ccsm.workerLogin(userId);
         await F.cache.validate();
         await F.foundation.initServiceWorker();
@@ -35,9 +33,12 @@ async function messageDrain() {
     await F.foundation.getMessageReceiver().drain();
 }
 
-const requestMessageDrain = _.debounce(() => F.queueAsync('fb-msg-handler', messageDrain), 1000);
-const fbm = firebase.messaging();
-fbm.setBackgroundMessageHandler(function(payload) {
-    requestMessageDrain();
-    return F.util.never(); // Prevent "site has been updated in back..."
-});
+if (F.env.FIREBASE_CONFIG) {
+    firebase.initializeApp(F.env.FIREBASE_CONFIG);
+    const fbm = firebase.messaging();
+    const requestMessageDrain = _.debounce(() => F.queueAsync('fb-msg-handler', messageDrain), 1000);
+    fbm.setBackgroundMessageHandler(function(payload) {
+        requestMessageDrain();
+        return F.util.never(); // Prevent "site has been updated in back..."
+    });
+}

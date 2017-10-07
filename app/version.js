d8d032444b9b8394e26ee8412eb1d454cc6cd9d6
Justin Mayfield <tooker@gmail.com>
2017-10-07 12:01:11 -0600

    Use safari compat libsignal

--- app/version.js
+++ app/version.js
@@ -2,7 +2,7 @@
 
 (function() {
     self.F = self.F || {};
-    F.version = '0.19.0';
+    F.version = '0.20.0';
     F.product = 'ForstaWeb';
 
     if (self.jQuery && (!F.env || F.env.STACK_ENV !== 'prod')) {

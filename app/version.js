7e737d88f307eecc6452eb133678b100a2d6c1e9
Justin Mayfield <tooker@gmail.com>
2017-12-18 15:27:58 -0700

    ver up 0.50.1

--- app/version.js
+++ app/version.js
@@ -2,7 +2,7 @@
 
 (function() {
     self.F = self.F || {};
-    F.version = '0.50.0';
+    F.version = '0.50.1';
     F.product = 'ForstaWeb';
     F.userAgent = [
         `${F.product}/${F.version}`,

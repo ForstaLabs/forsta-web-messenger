854cc3462411f1c6837da1b14894dbfd019e6109
Justin Mayfield <tooker@gmail.com>
2018-01-11 17:28:06 -0700

    librelay update for new signalserver

--- app/version.js
+++ app/version.js
@@ -2,7 +2,7 @@
 
 (function() {
     self.F = self.F || {};
-    F.version = '0.55.7';
+    F.version = '0.55.8';
     F.product = 'ForstaWeb';
     F.userAgent = [
         `${F.product}/${F.version}`,

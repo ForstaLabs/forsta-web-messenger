48745e9f500691cdc65a714567748ceb5e50abde
Justin Mayfield <tooker@gmail.com>
2018-01-23 18:43:36 -0700

    Warn if template helper for `ifeq` is used wrongly.

--- app/templates.js
+++ app/templates.js
@@ -157,6 +157,9 @@
     };
 
     ns.help.ifeq = function(left, right, options) {
+        if (typeof left !== typeof right) {
+            console.warn("Left and right values not same type:", left, right);
+        }
         if (left === right) {
             return options.fn(this);
         } else {

bf8e7725df642ade95e021b223880943f0d27ddb
Justin Mayfield <tooker@gmail.com>
2018-01-31 18:54:32 -0700

    Increase compat of ngram based searching.
    Fixes firefox

--- app/models/searchable.js
+++ app/models/searchable.js
@@ -49,34 +49,48 @@
             const tx = db.transaction(this.storeName);
             const modelProto = this.model.prototype;
             const store = tx.objectStore(this.storeName);
-            const jobs = modelProto.searchIndexes.map(x => ({
-                ngrams: modelProto.ngram(x.length, criteria),
-                index: store.index(x.index)
-            }));
-            const cursors = new Set();
-            for (const job of jobs) {
-                for (const ngram of job.ngrams) {
-                    cursors.add(job.index.openKeyCursor(ngram));
+            const cursorRequests = [];
+            for (const x of modelProto.searchIndexes) {
+                for (const key of modelProto.ngram(x.length, criteria)) {
+                    cursorRequests.push(store.index(x.index).openKeyCursor(key));
                 }
             }
             const matchCounts = new Map();
-            const requiredCount = cursors.size; // Perhaps could be percentage of ngrams for more fuzz?
+            const requiredCount = cursorRequests.length; // Perhaps could be percentage of ngrams for more fuzz?
             const matches = new Set();
-            while (cursors.size && matches.size < limit) {
-                for (const cursor of cursors) {
-                    const match = await F.util.idbRequest(cursor);
-                    if (match) {
-                        const pk = match.primaryKey;
-                        matchCounts.set(pk, (matchCounts.get(pk) || 0) + 1);
-                        if (matchCounts.get(pk) >= requiredCount) {
-                            matches.add(pk);
+            await new Promise((resolve, reject) => {
+                const activeRequests = new Set(cursorRequests);
+                let done;
+                for (const req of cursorRequests) {
+                    req.onsuccess = ev => {
+                        /* NOTE: this is an event callback for each match, not a one time deal! */
+                        if (done) {
+                            return;
                         }
-                        match.continue();
-                    } else {
-                        cursors.delete(cursor);
-                    }
+                        const cursor = ev.target.result;
+                        if (cursor) {
+                            const pk = cursor.primaryKey;
+                            matchCounts.set(pk, (matchCounts.get(pk) || 0) + 1);
+                            if (matchCounts.get(pk) >= requiredCount) {
+                                matches.add(pk);
+                                if (limit && matches.size === limit) {
+                                    done = true;
+                                    resolve();  // No need to continue;
+                                }
+                            }
+                            cursor.continue();
+                        } else {
+                            activeRequests.delete(req);
+                            if (!activeRequests.size) {
+                                resolve();  // Didn't reach limit.
+                            }
+                        }
+                    };
+                    req.onerror = ev => {
+                        reject(new Error(ev.target.errorCode));
+                    };
                 }
-            }
+            });
             const models = Array.from(matches).map(id => new this.model({id}));
             await Promise.all(models.map(m => m.fetch()));
             this.reset(models);

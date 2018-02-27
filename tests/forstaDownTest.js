7cb6db1a22998789c20204df518af7c3eb404071
Justin Mayfield <tooker@gmail.com>
2018-02-27 14:01:44 -0700

    New compliance tests for forstadown literals

--- tests/forstaDownTest.js
+++ tests/forstaDownTest.js
@@ -1,21 +1,18 @@
 
 function main() {
-  const fd = require('../lib/forstadown.js');
-  const json = require('./data.json');
-  for (let i = 0 ; i < json.length ; i++) {
-      let out = fd.inlineConvert(fd.blockConvert(json[i].input), new Set(["body"]));
-      if (out !== json[i].expected) {
-        console.log();
-        console.log(`Test (${json[i].name}) FAILED`);
-        console.log(`Expected: ${json[i].expected}`);
-        console.log(`Output: ${out}`);
-        console.log("Exiting...");
-        console.log();
-        process.exit(-1);
-      } else {
-        console.log(`Test (${json[i].name}) Passed`);
-      }
-  }
+    const fd = require('../lib/forstadown.js');
+    const json = require('./data.json');
+    for (let i = 0 ; i < json.length ; i++) {
+        let out = fd.inlineConvert(fd.blockConvert(json[i].input), new Set(["body"]));
+        if (out !== json[i].expected) {
+            console.error(`\nFAILED TEST ${i}: ${json[i].name}`);
+            console.error(`  Expected: "${json[i].expected}"`);
+            console.error(`  Actual  : "${out}"`);
+            process.exit(-1);
+        } else {
+            console.info(`PASSED TEST ${i}: ${json[i].name}`);
+        }
+    }
 }
 
 main();

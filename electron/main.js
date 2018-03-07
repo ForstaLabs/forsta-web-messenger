f33fb42206ef5c4b67d32374c1518ced5e978a1f
Justin Mayfield <tooker@gmail.com>
2018-03-07 16:02:19 -0700

    Prevent close of electron window / Add tray icon.
    Use hide/show instead so our websocket lives on no matter what.

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow, ipcMain, Tray, Menu} = require('electron');
 const menu = require('./menu')
+const path = require('path');
+const process = require('process');
+
 const port = Number(process.env['PORT']) || 10080;
+const icon = path.join(__dirname, '../images/app_icon_192.png');
 
 process.on('uncaughtException', function (err) {
     console.error(err);
@@ -12,15 +16,23 @@ process.on('uncaughtException', function (err) {
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+function showWindow() {
+    if (!win) {
+        createWindow();
+    } else {
+        win.show();
+    }
+}
+
 function createWindow() {
     console.info("Creating Window");
     win = new BrowserWindow({ 
         width: 1024,
         height: 768,
+        icon,
+        darkTheme: true,
         webPreferences: {
             nodeIntegration: false,
-            javascript: true,
-            plugins: true,
             preload: __dirname + '/renderer.js',
             experimentalFeatures: true
             // sandbox: true,
@@ -31,7 +43,10 @@ function createWindow() {
     win.loadURL(`http://localhost:${port}/@`);
     // win.webContents.openDevTools()
 
-    // Emitted when the window is closed.
+    win.on('close', ev => {
+        ev.preventDefault();
+        win.hide();  // Keep it alive to avoid closing our websocket.
+    });
     win.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
@@ -48,25 +63,21 @@ app.on('ready', () => {
     console.info("Starting server");
     require('../server/start');
     menu.setAppMenu();
+    const tray = new Tray(icon);
+    tray.setToolTip('Forsta Secure Messenger');
+    tray.on('click', showWindow);
     createWindow();
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if(process.platform !== 'darwin') {
-        app.quit();
+        //app.quit();
     }
 });
 
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (!win) {
-        createWindow();
-    }
-});
+app.on('activate', showWindow);
 
 // In main process.
 ipcMain.on('updateUnreadCount', (event, arg) => {

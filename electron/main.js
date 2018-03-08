2926df965580257984dff80e76d8b77a9f5cb72d
Justin Mayfield <tooker@gmail.com>
2018-03-07 17:20:25 -0700

    Different tray icons for osx.

--- electron/main.js
+++ electron/main.js
@@ -4,17 +4,33 @@ const {app, BrowserWindow, ipcMain, Tray, nativeImage} = require('electron');
 const menu = require('./menu');
 const path = require('path');
 const process = require('process');
+const platform = require('os').platform();
 
 const title = 'Forsta Messenger';
 const port = Number(process.env['PORT']) || 10080;
 const imagesDir = path.join(__dirname, '../images/');
-const icon = nativeImage.createFromPath(imagesDir + 'favicon.png');
-const iconPending = nativeImage.createFromPath(imagesDir + 'favicon-pending.png');
+const appIcon = nativeImage.createFromPath(imagesDir + 'app_icon_192.png');
+
+let trayIcon;
+let trayIconPending;
+if (platform === 'darwin') {
+    trayIcon = nativeImage.createFromPath(imagesDir + 'tray_icon.png');
+    trayIcon.setTemplateImage(true);
+    trayIconPending = nativeImage.createFromPath(imagesDir + 'tray_icon_pending.png');
+    trayIconPending.setTemplateImage(true);
+} else {
+    trayIcon = nativeImage.createFromPath(imagesDir + 'favicon.png');
+    trayIconPending = nativeImage.createFromPath(imagesDir + 'favicon-pending.png');
+}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 let tray;
+
+if (app.dock) {
+    app.dock.setIcon(appIcon);
+}
 
 function showWindow() {
     if (!win) {
@@ -29,7 +45,7 @@ function createWindow() {
     win = new BrowserWindow({ 
         width: 1024,
         height: 768,
-        icon,
+        icon: appIcon,
         title,
         darkTheme: true,
         webPreferences: {
@@ -63,12 +79,14 @@ app.on('ready', () => {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    console.info("Starting server");
     require('../server/start');
+
     menu.setAppMenu();
-    tray = new Tray(icon);
+
+    tray = new Tray(trayIcon);
     tray.setToolTip(title);
     tray.on('click', showWindow);
+
     createWindow();
 });
 app.on('before-quit', () => {
@@ -82,7 +100,7 @@ app.on('activate', showWindow);
 // Handle events sent from the browser...
 ipcMain.on('updateUnreadCount', (event, count) => {
     app.setBadgeCount(count);
-    tray.setImage(count ? iconPending : icon);
+    tray.setImage(count ? trayIconPending : trayIcon);
     tray.setToolTip(count ? `${count} unread messages` : title);
 });
 ipcMain.on('showWindow', () => showWindow());

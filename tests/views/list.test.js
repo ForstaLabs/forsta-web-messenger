348d93a12fa21a88350abcef37ba100e2672c1d4
Justin Mayfield <tooker@gmail.com>
2019-05-19 15:24:18 -0600

    Tests for ListView bugs recently fixed

--- tests/views/list.test.js
+++ tests/views/list.test.js
@@ -29,6 +29,118 @@ describe('ListView tests', () => {
             if (view.getItems().length !== 3) {
                 throw new Error('expected 3 items to render');
             }
+            await view.attachingItems;
+            for (const item of view.getItems()) {
+                if (!view.$(item.$el).length) {
+                    throw new Error("Item not added to DOM");
+                }
+            }
         });
     });
+
+    it('can handle sync nosuper item render', async () => {
+        await page.evaluate(async () => {
+            const ItemView = F.View.extend({
+                render: () => 'nothing doing'
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 3) {
+                throw new Error('expected 3 items to render');
+            }
+            await view.attachingItems;
+            for (const item of view.getItems()) {
+                if (!view.$(item.$el).length) {
+                    throw new Error("Item not added to DOM");
+                }
+            }
+        });
+    });
+
+    it('can handle async nosuper item render', async () => {
+        await page.evaluate(async () => {
+            const ItemView = F.View.extend({
+                render: async () => { await F.sleep(0.001); }
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 3) {
+                throw new Error('expected 3 items to render');
+            }
+            await view.attachingItems;
+            for (const item of view.getItems()) {
+                if (!view.$(item.$el).length) {
+                    throw new Error("Item not added to DOM");
+                }
+            }
+        });
+    });
+
+    it('can handle sync exception during item render', async () => {
+        await page.evaluate(async () => {
+            let i = 0;
+            const ItemView = F.View.extend({
+                render: function() {
+                    if (i++ === 1) {
+                        this.threw = true;
+                        throw new Error("boom");
+                    }
+                }
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 3) {
+                throw new Error('expected 3 items to render');
+            }
+            await view.attachingItems;
+            for (const item of view.getItems()) {
+                if (!view.$(item.$el).length) {
+                    if (!item.threw) {
+                        throw new Error("Item not added to DOM");
+                    }
+                } else if (item.threw) {
+                    throw new Error("Item should not have been added to DOM");
+                }
+            }
+        });
+    });
+
+    it('can handle async exception during item render', async () => {
+        await page.evaluate(async () => {
+            let i = 0;
+            const ItemView = F.View.extend({
+                render: async function() {
+                    if (i++ === 1) {
+                        await F.sleep(0.01);
+                        this.threw = true;
+                        throw new Error("boom");
+                    }
+                }
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 3) {
+                throw new Error('expected 3 items to render');
+            }
+            await view.attachingItems;
+            for (const item of view.getItems()) {
+                if (!view.$(item.$el).length) {
+                    if (!item.threw) {
+                        throw new Error("Item not added to DOM");
+                    }
+                } else if (item.threw) {
+                    throw new Error("Item should not have been added to DOM");
+                }
+            }
+        });
+    });
+
 });

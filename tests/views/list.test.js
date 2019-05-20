4f7095befeb1daafc9ca9c4b3d2e1d9739fb040e
Justin Mayfield <tooker@gmail.com>
2019-05-20 15:15:35 -0600

    Add/clean listview tests.
    Testing more conditions.

--- tests/views/list.test.js
+++ tests/views/list.test.js
@@ -1,6 +1,8 @@
 /* global Backbone */
 
 const util = require('../util');
+const process = require('process');
+const testTimeout = process.env.HEADLESS === 'false' ? 3600 * 1000 : undefined;
 
 describe('ListView tests', () => {
     beforeAll(async () => {
@@ -12,13 +14,13 @@ describe('ListView tests', () => {
     it('exists', async () => {
         const exists = await page.evaluate(() => !!F.ListView);
         expect(exists).toBe(true);
-    });
+    }, testTimeout);
 
     it('can be instantiated', async () => {
         await page.evaluate(() => {
             new F.ListView({});
         });
-    });
+    }, testTimeout);
 
     it('can render', async () => {
         await page.evaluate(async () => {
@@ -30,13 +32,11 @@ describe('ListView tests', () => {
                 throw new Error('expected 3 items to render');
             }
             await view.attachingItems;
-            for (const item of view.getItems()) {
-                if (!view.$(item.$el).length) {
-                    throw new Error("Item not added to DOM");
-                }
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
             }
         });
-    });
+    }, testTimeout);
 
     it('can handle sync nosuper item render', async () => {
         await page.evaluate(async () => {
@@ -51,13 +51,51 @@ describe('ListView tests', () => {
                 throw new Error('expected 3 items to render');
             }
             await view.attachingItems;
-            for (const item of view.getItems()) {
-                if (!view.$(item.$el).length) {
-                    throw new Error("Item not added to DOM");
-                }
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
             }
         });
-    });
+    }, testTimeout);
+
+    it('can handle falsy async render return value', async () => {
+        await page.evaluate(async () => {
+            const returns = [null, undefined, 0, '', []];
+            const ItemView = F.View.extend({
+                render: async function() { return returns[this.id - 1]; }
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 5) {
+                throw new Error('expected 5 items to render');
+            }
+            await view.attachingItems;
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
+            }
+        });
+    }, testTimeout);
+
+    it('can handle falsy sync render return value', async () => {
+        await page.evaluate(async () => {
+            const returns = [null, undefined, 0, '', []];
+            const ItemView = F.View.extend({
+                render: function() { return returns[this.id - 1]; }
+            });
+            const collection = new Backbone.Collection();
+            collection.add([{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]);
+            const view = new F.ListView({collection, ItemView});
+            await view.render();
+            if (view.getItems().length !== 5) {
+                throw new Error('expected 5 items to render');
+            }
+            await view.attachingItems;
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
+            }
+        });
+    }, testTimeout);
 
     it('can handle async nosuper item render', async () => {
         await page.evaluate(async () => {
@@ -72,13 +110,11 @@ describe('ListView tests', () => {
                 throw new Error('expected 3 items to render');
             }
             await view.attachingItems;
-            for (const item of view.getItems()) {
-                if (!view.$(item.$el).length) {
-                    throw new Error("Item not added to DOM");
-                }
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
             }
         });
-    });
+    }, testTimeout);
 
     it('can handle sync exception during item render', async () => {
         await page.evaluate(async () => {
@@ -99,13 +135,11 @@ describe('ListView tests', () => {
                 throw new Error('expected 3 items to render');
             }
             await view.attachingItems;
-            for (const item of view.getItems()) {
-                if (!view.$(item.$el).length) {
-                    throw new Error("Item not added to DOM");
-                }
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
             }
         });
-    });
+    }, testTimeout);
 
     it('can handle async exception during item render', async () => {
         await page.evaluate(async () => {
@@ -127,12 +161,9 @@ describe('ListView tests', () => {
                 throw new Error('expected 3 items to render');
             }
             await view.attachingItems;
-            for (const item of view.getItems()) {
-                if (!view.$(item.$el).length) {
-                    throw new Error("Item not added to DOM");
-                }
+            if (!view.getItems().every(x => view.isAttached(x))) {
+                throw new Error("Item not added to DOM");
             }
         });
-    });
-
+    }, testTimeout);
 });

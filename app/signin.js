23b930327c185ac840a400fa7f959315001fed29
Justin Mayfield <tooker@gmail.com>
2019-06-10 16:36:50 -0600

    Update cache init for signin page

--- app/signin.js
+++ app/signin.js
@@ -1,10 +1,11 @@
 // vim: ts=4:sw=4:expandtab
+/* global Backbone */
 
 (function() {
     'use strict';
 
     async function main() {
-        await F.cache.startSharedCache();
+        await Backbone.initDatabase(F.SharedCacheDatabase);
         await F.tpl.loadPartials();
         F.signinView = new F.SigninView({el: $('body')});
         await F.signinView.render();

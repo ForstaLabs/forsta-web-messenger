836c5eb2589e82ae946e6de4b0f13d31f1830f80
Justin Mayfield <tooker@gmail.com>
2019-11-05 13:04:56 -0700

    Remove deprecated signin test

--- tests/sanity.test.js
+++ tests/sanity.test.js
@@ -12,8 +12,4 @@ describe('Sanity Tests', () => {
     it('can parse embed script', async () => {
         await page.addScriptTag({path: 'dist/static/js/app/embed.js'});
     });
-
-    it('can parse signin script', async () => {
-        await page.addScriptTag({path: 'dist/static/js/app/signin.js'});
-    });
 });
